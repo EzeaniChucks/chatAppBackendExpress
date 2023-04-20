f59985d63818a6633e538b648c612aedb300ff6d
EzeaniChucks <concord_chucks2@yahoo.com>
2023-04-19 23:39:59 -0700

    finally fixed app

--- index.js
+++ index.js
@@ -16,21 +16,18 @@ const port = process.env.PORT || 8081;
 app.use(cors())
 app.use(express.json())
 
-app.post('/',async(req, res)=>{
-    const {messages} = req.body;
-    console.log(messages)
-    try{
-
-        const completion = await openAi.createChatCompletion({
-            model:'gpt-3.5-turbo',
-            max_tokens: 3000,
-            messages
-        });
-        return res.status(200).json(completion.data.choices[0].message.content);
-    }catch(err){
-        return res.status(500).json(err.message)
-    }
-})
+app.post("/", async (req, res) => {
+  try {
+    const completion = await openAi.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      max_tokens: 3000,
+      messages: req?.body,
+    });
+    return res.status(200).json(completion.data.choices[0].message.content);
+  } catch (err) {
+    return res.status(500).json(err.message);
+  }
+});
 
 app.listen(port, ()=>{
     console.log(`app is listening on ${port}`)
